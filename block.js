68b626dbf4d17bd6a93fbb614f8e30d5a83090c1
Norman Zhang <norman.x.zhang@gmail.com>
2025-02-18 00:08:44 -0700

    process urls from JS rather than matching from manifest

--- block.js
+++ block.js
@@ -1,4 +1,17 @@
-window.location.replace("https://procrastination.com/");
+// Get list
+// Tokenize words and compare
+// go to random page
+
+let isinvalid = window.location.href.localeCompare("https://www.twitch.tv/");
 
 
+if (isinvalid===0) {
+    window.location.replace("https://procrastination.com/");
+    console.log("Going to counter website   ")
+}
+
+/*
+doesn't register clicks from twitch.tv/streamer -> twitch.tv
+refreshes + direct website 
+*/
 // https://procrastination.com/
